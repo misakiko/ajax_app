d371210f4b646c498415bfbc9b0e621c3c245e40
misakiko <34misaki.k@gmail.com>
2022-06-21 18:54:06 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   // 関数宣言
   const submit = document.getElementById("submit");
@@ -18,10 +32,25 @@ function post (){
     // レスポンスの型  
     XHR.send(formData);
     // formdataに代入されているフォームに入力した値を送信する
+    XHR.onload = () => {
+      // レスポンスの受信に成功
+      // console.log(XHR.response);
+        // レスポンスの内容確認
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;
+        };
+        // レスポンスに問題があったとき
+      const list = document.getElementById("list"); 
+      // 新しいメモを挿入するための要素取得 listに格納 
+      const formText = document.getElementById("content");
+      // 投稿するたびにフォームの値リセット
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+        // ある要素の指定箇所にhtml挿入
+        formText.value = "";
+    };
   });
-
  };
- 
  window.addEventListener('load', post);
 //  事前に読み込む post 
 // windowでページ読み込んで関数post実行 先頭のpostにいく
